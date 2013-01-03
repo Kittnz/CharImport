fa57d8026e97a90e3fe1d0d38005e2fe3d73bec3
megasus <geslauncher@web.de>
2013-01-03 20:26:25 +0100

    Indev changes

--- CharImport/Template_Database.Designer.vb
+++ CharImport/Template_Database.Designer.vb
@@ -22,8 +22,90 @@ Partial Class Template_Database
     'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.templatename = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.description = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.Creator = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.downloads = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.SuspendLayout()
+        '
+        'ListView1
+        '
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.templatename, Me.description, Me.Creator, Me.downloads})
+        Me.ListView1.Location = New System.Drawing.Point(12, 57)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(982, 438)
+        Me.ListView1.TabIndex = 0
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'Button8
+        '
+        Me.Button8.BackColor = System.Drawing.Color.DimGray
+        Me.Button8.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button8.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.Button8.Location = New System.Drawing.Point(855, 512)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(139, 39)
+        Me.Button8.TabIndex = 152
+        Me.Button8.Text = "Zurück"
+        Me.Button8.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.DimGray
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.Button1.Location = New System.Drawing.Point(325, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(139, 39)
+        Me.Button1.TabIndex = 153
+        Me.Button1.Text = "Suche"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'templatename
+        '
+        Me.templatename.Text = "Name"
+        Me.templatename.Width = 120
+        '
+        'description
+        '
+        Me.description.Text = "Beschreibung"
+        Me.description.Width = 500
+        '
+        'Creator
+        '
+        Me.Creator.Text = "Ersteller"
+        Me.Creator.Width = 120
+        '
+        'downloads
+        '
+        Me.downloads.Text = "Downloads"
+        Me.downloads.Width = 80
+        '
+        'Template_Database
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(38, Byte), Integer), CType(CType(50, Byte), Integer), CType(CType(82, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(1006, 563)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.ListView1)
+        Me.Name = "Template_Database"
         Me.Text = "Template_Database"
+        Me.ResumeLayout(False)
+
     End Sub
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+    Friend WithEvents Button8 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents templatename As System.Windows.Forms.ColumnHeader
+    Friend WithEvents description As System.Windows.Forms.ColumnHeader
+    Friend WithEvents Creator As System.Windows.Forms.ColumnHeader
+    Friend WithEvents downloads As System.Windows.Forms.ColumnHeader
 End Class
--- CharImport/Template_Database.vb
+++ CharImport/Template_Database.vb
@@ -1,3 +1,48 @@
-﻿Public Class Template_Database
+﻿Imports MySql.Data.MySqlClient
+Imports MySqlLib3
+Imports System.Text
+Public Class Template_Database
+    Private db As New MySqlLib("http://wowgeslauncher.bplaced.com/SQLScript/query.php", True)
+    Private Sub Template_Database_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        loadtemplatelist()
+    End Sub
+    Private Sub loadtemplatelist()
+        db.Encoding = Encoding.GetEncoding("ISO-8859-1")
+        Dim returnvalue As String = ""
+        Try
+            Dim rc As MySqlLib.ResultCollection = db.Query("SELECT * FROM `template_store`")
+            Dim rowcount As Integer = rc.Row.Count
+            Dim numrow As Integer = 0
+            Dim listcolcount As Integer = 0
+            If rc.Row.Count > 0 Then
+                Do
+                    For Each kp As Generic.KeyValuePair(Of String, String) In rc.Row(numrow).Columns
+                        returnvalue &= kp.Key & " -> " & kp.Value & Environment.NewLine
+                    Next
+                    Dim str(3) As String
+                    Dim itm As ListViewItem
+                    str(0) = globalregion.SelectedItem.ToString
+                    str(1) = realmname.Text
+                    str(2) = charname.Text
+                    str(3) = templink
+                    itm = New ListViewItem(str)
+                    ListView1.Items.Add(itm)
+                    ListView1.EnsureVisible(ListView1.Items.Count - 1)
+                    ListView1.Update()
+                    listcolcount = 0
+                    numrow += 1
+                Loop Until numrow = rowcount
+            End If
+        Catch scriptEx As MySqlLib.ScriptException
+            MessageBox.Show(scriptEx.Message, "Script Fehler")
+        Catch mysqlEx As MySqlLib.MySqlException
+            MessageBox.Show(mysqlEx.ToShortString(), "MySql Fehler")
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Allgemeiner Fehler")
+        End Try
+        If Not returnvalue = "" Then
 
+
+        End If
+    End Sub
 End Class
